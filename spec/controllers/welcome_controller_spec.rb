e4db99fe26c826f53e7fbcb8b10fc4b0bf076cc0
Jason Li <jason.li0102@gmail.com>
2017-06-07 11:00:46 -0700

    Set up user sign up and confirmation with Devise Gem

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+  let(:user) { create(:user) }
+
   describe "GET #index" do
     it "renders the index template" do
+      user.confirm
+      sign_in(user)
       get :index
       expect(response).to render_template(:index)
     end
@@ -11,9 +15,10 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #about" do
     it "renders the about template" do
+      user.confirm
+      sign_in(user)
       get :about
       expect(response).to render_template(:about)
     end
   end
-
 end
