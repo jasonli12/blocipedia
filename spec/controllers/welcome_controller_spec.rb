0b4cc90e28afe8fc26ac9de5323262fd94f7a5c3
Jason Li <jason.li0102@gmail.com>
2017-06-07 19:51:52 -0700

    Add skip_before_action :authenticate_user! to WelcomeController and update associated tests

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
     it "renders the index template" do
-      user.confirm
-      sign_in(user)
       get :index
       expect(response).to render_template(:index)
     end
@@ -15,8 +13,6 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #about" do
     it "renders the about template" do
-      user.confirm
-      sign_in(user)
       get :about
       expect(response).to render_template(:about)
     end
