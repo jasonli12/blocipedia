24181728cf94178c1119d17a4ba8bfcb3f7c61b2
Jason Li <jason.li0102@gmail.com>
2017-07-16 19:28:38 -0700

    Allow users to use markdown in wikis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    wikis_path #'/wikis'
+    wikis_path
   end
 end
